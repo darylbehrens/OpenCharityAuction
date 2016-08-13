b9abd785ece2bc9949e557cf29218d25ad5a5bf8
daryl <darylbehrens@gmail.com>
2016-08-13 10:14:05 -0700

    Initial Commit

---
+++ src/OpenCharityAuction/Models/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenCharityAuction.Models
+{
+    public class Configuration
+    {
+        public string AppName { get; }
+
+        public Configuration(string _appName)
+        {
+            AppName = _appName;
+        }
+    }
+}
--- src/OpenCharityAuction/Startup.cs
+++ src/OpenCharityAuction/Startup.cs
@@ -5,17 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace OpenCharityAuction
 {
     public class Startup
     {
+        public IConfigurationRoot config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("config.json");
+
+            config = builder.Build();
+
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(new Models.Configuration(config["app:appName"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,7 +45,8 @@ namespace OpenCharityAuction
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var myConfig = context.RequestServices.GetRequiredService<Models.Configuration>();
+                await context.Response.WriteAsync(myConfig.AppName);
             });
         }
     }
