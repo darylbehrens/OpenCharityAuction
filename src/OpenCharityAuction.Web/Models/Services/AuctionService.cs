44c733b48a3069c9975a7715b939878503e785d4
daryl <darylbehrens@gmail.com>
2016-08-14 14:54:22 -0700

    Added Auction Data Context Service and Home Controller

---
+++ src/OpenCharityAuction.Web/Models/Services/AuctionService.cs
@@ -0,0 +1,18 @@
+﻿using OpenCharityAuction.Web.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenCharityAuction.Web.Models.Services
+{
+    public class AuctionService : IAuctionService
+    {
+        private readonly Data.AuctionContext AuctionContext;
+
+        public AuctionService(Data.AuctionContext context)
+        {
+            AuctionContext = context;
+        }
+    }
+}
