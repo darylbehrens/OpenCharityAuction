f5b67a54823b2718e708921fcb867e9e91f20a8b
daryl <darylbehrens@gmail.com>
2016-09-06 18:44:37 -0700

    Changed SelectCurrentEvent to Angular Form

--- src/OpenCharityAuction.Web/wwwroot/scripts/angular/search/search.js
+++ src/OpenCharityAuction.Web/wwwroot/scripts/angular/search/search.js
@@ -1,11 +1,26 @@
 var search = angular.module('search', []);
 
-search.controller('SearchController', function SearchController($scope, $http) {
+search.controller('MealSearchController', function SearchController($scope, $http) {
     $http({
         method: "GET",
         url: "/api/meals"
     }).then(function success(response) {
         $scope.items = response.data;
         console.log($scope.items);
-    })
+    });
+});
+
+search.controller('EventSearchController', function SearchController($scope, $http) {
+    $http({
+        method: "GET",
+        url: "/api/events"
+    }).then(function success(response) {
+        $scope.items = response.data;
+        console.log($scope.items);
+    });
+
+    $scope.post = function (eventId) {
+
+    }
+
 });
